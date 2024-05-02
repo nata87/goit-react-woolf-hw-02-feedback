fb44812d34a21fef55f47bfb8746083bccc9fe3e
Nataliya Pustelnyk <nata_pustelnyk@ukr.net>
2024-05-02 21:42:16 +0200

    step 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,33 @@
+import { useState } from 'react';
+
 export const App = () => {
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
   return (
     <div
       style={{
         height: '100vh',
         display: 'flex',
+        flexDirection: 'column',
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
-      React homework template
+      <h1>Please leave feedback</h1>
+      <div>
+        <button>Good</button>
+        <button>Neutral</button>
+        <button>Bad</button>
+      </div>
+      <h2>Statistics</h2>
+      <div>Good: {state.good}</div>
+      <div>Neutral: {state.neutral}</div>
+      <div>Bad: {state.bad}</div>
     </div>
   );
 };
