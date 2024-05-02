1496f2a545a07827fa223541531feb98f428cbc6
Nataliya Pustelnyk <nata_pustelnyk@ukr.net>
2024-05-02 22:23:19 +0200

    step 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,70 @@
+import { useEffect } from 'react';
 import { useState } from 'react';
+import styles from './app.module.css';
 
 export const App = () => {
   const [state, setState] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+    total: 0,
+    percentage: 0,
   });
+
+  const goodFeedback = () => {
+    const good = state.good + 1;
+    setState(prev => {
+      return {
+        ...prev,
+        good,
+      };
+    });
+  };
+
+  const neutralFeedback = () => {
+    const neutral = state.neutral + 1;
+    setState(prev => {
+      return {
+        ...prev,
+        neutral,
+      };
+    });
+  };
+
+  const badFeedback = () => {
+    const bad = state.bad + 1;
+    setState(prev => {
+      return {
+        ...prev,
+        bad,
+      };
+    });
+  };
+
+  const countTotalFeedback = () => {
+    const total = state.good + state.neutral + state.bad;
+    setState(prev => {
+      return {
+        ...prev,
+        total,
+      };
+    });
+  };
+  const countPositiveFeedbackPercentage = () => {
+    const percentage = Math.round((state.good / state.total) * 100, 2);
+    setState(prev => {
+      return {
+        ...prev,
+        percentage,
+      };
+    });
+  };
+
+  useEffect(() => {
+    countTotalFeedback();
+    if (state.total) countPositiveFeedbackPercentage();
+  }, [state.good, state.neutral, state.bad]);
+
   return (
     <div
       style={{
@@ -19,15 +78,23 @@ export const App = () => {
       }}
     >
       <h1>Please leave feedback</h1>
-      <div>
-        <button>Good</button>
-        <button>Neutral</button>
-        <button>Bad</button>
+      <div className={styles.container}>
+        <button className={styles.button} onClick={goodFeedback}>
+          Good
+        </button>
+        <button className={styles.button} onClick={neutralFeedback}>
+          Neutral
+        </button>
+        <button className={styles.button} onClick={badFeedback}>
+          Bad
+        </button>
       </div>
       <h2>Statistics</h2>
       <div>Good: {state.good}</div>
       <div>Neutral: {state.neutral}</div>
       <div>Bad: {state.bad}</div>
+      <div>Total: {state.total}</div>
+      <div>Positive Feedback: {state.percentage} %</div>
     </div>
   );
 };
